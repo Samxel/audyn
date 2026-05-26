492402c67d9aad623c1bb6675f13a058e95b7cfa
Samuel L. <72248110+Samxel@users.noreply.github.com>
2026-05-26 22:38:52 +0200

    cache caps xml on first load

--- handler/caps.go
+++ handler/caps.go
@@ -6,22 +6,31 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"sync"
 )
 
 type CapsHandler struct {
 	Config config.Config
 }
 
+var (
+	capsOnce  sync.Once
+	capsBytes []byte
+	capsErr   error
+)
+
 func (h *CapsHandler) Serve(w http.ResponseWriter, r *http.Request) {
 	slog.Info("Caps requested")
-	dat, err := os.ReadFile("config/caps.xml")
-	if err != nil {
-		slog.Error("caps.xml not found", "err", err)
+	capsOnce.Do(func() {
+		capsBytes, capsErr = os.ReadFile("config/caps.xml")
+	})
+	if capsErr != nil {
+		slog.Error("caps.xml not found", "err", capsErr)
 		w.Header().Set("Content-Type", "application/xml; charset=utf-8")
 		fmt.Fprint(w, `<?xml version="1.0" encoding="UTF-8"?><error code="300" description="caps.xml not found"/>`)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
-	w.Write(dat)
+	w.Write(capsBytes)
 }
